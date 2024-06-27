cec836b4d6a340ab1c91beb20336bb080047ac71
starht <star2thb@gmail.com>
2024-06-27 10:25:03 +0900

    CICD

--- src/main/java/testrds/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package testrds.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import testrds.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
